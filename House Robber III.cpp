9bf94930dd1efd12091ad15128cb827ee4de1a59
Trang Nguyen <trangnmnguyen@gmail.com>
2018-07-17 23:04:54 -0400

    more problem solved

--- House Robber III.cpp
+++ House Robber III.cpp
@@ -10,6 +10,24 @@
 class Solution {
 public:
     int rob(TreeNode* root) {
-        
+        int sumOdd = 0, sumEven = root->val, level = 1;
+        recursive(root, sumOdd, sumEven, level);
+        if(sumEven >= sumOdd) return sumEven;
+        return sumOdd;
+    }
+
+    void recursive(TreeNode* root, int &sumOdd, int &sumEven, int level) {
+    	if(!root) return;
+
+    	if(level % 2 == 1) {
+    		sumOdd += root->val;
+    	} else {
+    		sumEven += root->val;
+    	}
+    	
+    	recursive(root->left, sumOdd, sumEven, level + 1);
+    	recursive(root->right, sumOdd, sumEven, level + 1);
+    	
+    	return;
     }
 };
---
+++ Minimum Size Subarray Sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
+        int res = 0, int sum = 0;
+        for(int i = 0; i < nums.size(); i++) {
+        	if(sum < s) {
+        		res++;
+        		
+        	} else {
+
+        	}
+        }
+    }
+};
